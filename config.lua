b0d49965460174829b8a6614a99be28dc3184f2f
MikeyUchiha <mikey@starfantasygames.com>
2015-04-20 12:29:16 -0700

    Changed the config.lua to the new Modernized version.

--- config.lua
+++ config.lua
@@ -1,19 +1,21 @@
+--calculate the aspect ratio of the device:
+local aspectRatio = display.pixelHeight / display.pixelWidth
+
 application =
 {
 
 	content =
 	{
-		width = 768,
-		height = 1024, 
+		width = aspectRatio > 1.5 and 320 or math.ceil( 480 / aspectRatio ),
+		height = aspectRatio < 1.5 and 480 or math.ceil( 320 * aspectRatio ), 
 		scale = "letterBox",
 		fps = 30,
 		
-		--[[
 		imageSuffix =
 		{
-			    ["@2x"] = 2,
+			    ["@2x"] = 1.5,
+                            ["@4x"] = 3.0,
 		},
-		--]]
 	},
 
 	--[[
